8b7bb7887bd5f383a8c82f747514ffdb7a683fe3
Mateusz Kierznowski <mateusz.kierznowski@nask.pl>
2019-02-26 16:07:12 +0100

    poprawka po ostatnim commicie

--- src/test/java/pl/nask/agent/component/database/data/entity/MojaKlasaTestowa.java
+++ src/test/java/pl/nask/agent/component/database/data/entity/MojaKlasaTestowa.java
@@ -3,13 +3,11 @@ package pl.nask.agent.component.database.data.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import pl.nask.agent.common.core.domain.AbstractDatabaseEntry;
-import pl.nask.agent.common.core.domain.IEntity;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class MojaKlasaTestowa extends AbstractDatabaseEntry implements IEntity {
+public class MojaKlasaTestowa {
 
     private String name;
     private String lastName;
